46248adc7e9b750e0007e5d9b39584c64f27a1ac
Taras Halynskyi <thalynskyi@codevision.com.ua>
2019-10-17 14:06:55 +0000

    IMP-1436: No changes are made when searching again

--- browser/src/app/entity/search/search.component.ts
+++ browser/src/app/entity/search/search.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CompanyInfo, ServiceType } from '../../model/data-model';
 import { CompanyService } from '../../api/services/company.service';
-import { Subscription } from 'rxjs';
+import { combineLatest, ReplaySubject, Subject } from 'rxjs';
 import { PopUpMessageService } from '../../util/pop-up-message.service';
 import { RestPage } from '../../api/models/RestPage';
 import { ServiceTypeService } from '../../api/services/service-type.service';
@@ -10,6 +10,7 @@ import { ProjectActionService } from '../../util/project-action.service';
 import * as lunr from 'lunr';
 import { getErrorMessage } from "../../util/functions";
 import { CustomerSuggestionService } from "../../api/services/customer-suggestion.service";
+import { finalize, first, takeUntil } from "rxjs/operators";
 
 
 @Component({
@@ -19,52 +20,88 @@ import { CustomerSuggestionService } from "../../api/services/customer-suggestio
 })
 export class SearchComponent implements OnInit, OnDestroy {
 
+  private readonly destroyed$ = new Subject<void>();
+  private readonly initialized$ = new ReplaySubject<void>(1);
+  public initialized = false;
+
   service: string;
   zipCode: string;
   notFound: boolean;
   searchResults: Array<ServiceType> = [];
   pageable: RestPage<CompanyInfo> = new RestPage<CompanyInfo>();
   loading = false;
-
   private page = 1;
   private size = 35;
-  private search: string;
+  private searchTerm: string;
   private zip: string;
-  private activatedRoute$: Subscription;
-  private companyService$: Subscription;
   private lunrIndex;
   private serviceTypes: Array<ServiceType>;
   popularServiceTypes: Array<ServiceType> = [];
 
   constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
               private companyService: CompanyService,
               public projectActionService: ProjectActionService,
               public customerSuggestionService: CustomerSuggestionService,
               private popUpService: PopUpMessageService,
               private serviceTypeService: ServiceTypeService) {
-    this.getPopularServices();
+
+    this.init();
+
+    this.activatedRoute.queryParams
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((params: Params) => {
+        if (this.initialized) {
+          this.retrieveParams(params);
+        } else {
+          this.initialized$
+            .pipe(first())
+            .subscribe(() => {
+              this.initialized = true;
+              this.retrieveParams(params);
+            })
+        }
+      });
+  }
+
+  retrieveParams(params: Params) {
+    this.service = params['service'] ? params['service'] : '';
+    this.zipCode = params['zip'] ? params['zip'] : '';
+    this.deepSearch(this.service, this.zipCode)
   }
 
   ngOnInit(): void {
-    this.activatedRoute$ = this.activatedRoute.queryParams.subscribe((params: Params) => {
-      this.service = params['service'] ? params['service'] : '';
-      this.zipCode = params['zip'] ? params['zip'] : '';
-      this.indexServiceTypes();
-    });
+
   }
 
-  getPopularServices() {
-    this.customerSuggestionService.popular$.subscribe(
-      popularServiceTypes => this.popularServiceTypes = popularServiceTypes,
-      err => this.popUpService.showError(getErrorMessage(err))
-    )
+  init() {
+    this.loading = true;
+    let requests = combineLatest([this.customerSuggestionService.popular$, this.serviceTypeService.serviceTypes$])
+      .pipe(
+        first(),
+        finalize(() => this.loading = false)
+      )
+      .subscribe(result => {
+          this.popularServiceTypes = result[0];
+          let services = this.serviceTypes = result[1];
+          this.lunrIndex = lunr(function () {
+            this.ref('id');
+            this.field('name');
+            services.forEach(service => this.add(service));
+          });
+          this.loading = false;
+          this.initialized$.next();
+          this.initialized$.complete();
+        },
+        err => this.popUpService.showError(getErrorMessage(err)))
+
   }
 
-  deepSearch(data: { service: string, zip: string }) {
-    this.zip = data.zip;
-    this.search = data.service;
+  deepSearch(searchTerm: string, zip: string) {
+    this.zip = zip;
+    this.searchTerm = searchTerm;
     if (this.lunrIndex) {
-      this.searchResults = this.lunrIndex.search(`${this.search}*`)
+      this.searchResults = this.lunrIndex.search(`${this.searchTerm}*`)
         .map(item => this.serviceTypes.find(service => item.ref == service.id))
         .filter((el, index) => index <= this.page * this.size);
     }
@@ -76,36 +113,14 @@ export class SearchComponent implements OnInit, OnDestroy {
 
   loadMore(): void {
     this.page++;
-    this.searchResults = this.lunrIndex.search(`${this.search}*`)
+    this.searchResults = this.lunrIndex.search(`${this.searchTerm}*`)
       .map(item => this.serviceTypes.find(service => item.ref == service.id))
       .filter((el, index) => index <= this.page * this.size);
   }
 
   ngOnDestroy(): void {
-    this.activatedRoute$.unsubscribe();
-    if (this.companyService$) {
-      this.companyService$.unsubscribe();
-    }
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
-  private indexServiceTypes() {
-    this.loading = true;
-    if (!this.lunrIndex) {
-      this.serviceTypeService.serviceTypes$
-        .subscribe((services: Array<ServiceType>) => {
-            this.serviceTypes = services;
-            this.lunrIndex = lunr(function () {
-              this.ref('id');
-              this.field('name');
-              services.forEach(service => this.add(service));
-            });
-            this.deepSearch({service: this.service, zip: this.zipCode});
-            this.loading = false;
-          },
-          err => {
-            this.popUpService.showError(getErrorMessage(err));
-            this.loading = false
-          })
-    }
-  }
 }
