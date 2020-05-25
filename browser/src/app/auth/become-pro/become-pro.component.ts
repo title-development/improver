8488b51f5e9e3dcb112d918dd836db46e3adb1cf
Ivan Soltys <soltisivan11@gmail.com>
2020-05-25 12:07:32 +0000

    IMP-1593: Implement Benefits, Configuration Hints, and Get Started UI blocks on Become a PRO page

--- browser/src/app/auth/become-pro/become-pro.component.ts
+++ browser/src/app/auth/become-pro/become-pro.component.ts
@@ -1,10 +1,13 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { SecurityService } from '../security.service';
 import { Constants } from '../../util/constants';
 import { Messages } from 'app/util/messages';
 import { GoogleAnalyticsService } from '../../util/google-analytics.service';
 import { HotJarService } from '../../util/hotjar.service';
 import { ScrollService } from "../../util/scroll.service";
+import { MediaQuery, MediaQueryService } from "../../util/media-query.service";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
   selector: 'become-pro-page',
@@ -14,6 +17,18 @@ import { ScrollService } from "../../util/scroll.service";
 
 
 export class BecomeProComponent implements AfterViewInit {
+
+  selectedBenefitImageUrl: string = 'benefit_1.png';
+	proBannerConfig: any = {
+		image: 'become-pro-banner.png',
+		title: 'Ready to get started?',
+		text: 'After you register free Home Improve business profile, you\'ll be set up to start receiving lead notifications for jobs in your area, within your scope of work',
+		button: 'Get started',
+		routerLink: '/signup-pro'
+	};
+	swiper: Swiper;
+	mediaQuery: MediaQuery;
+	private readonly destroyed$: Subject<void> = new Subject();
 
   testimonials = [
     {
@@ -36,17 +51,98 @@ export class BecomeProComponent implements AfterViewInit {
     },
   ];
 
+  benefits = [
+    {
+      title: 'Reliable payment',
+      text: 'You only pay for the leads you receive, and if you get any bad lead we’ll return your money back',
+      desktopImage: 'benefit_1.png',
+			mobileImage: 'mobile_benefit_1.png'
+    },
+    {
+      title: 'Free choice',
+      text: 'Choose exactly which leads to buy',
+			desktopImage: 'benefit_2.png',
+			mobileImage: 'mobile_benefit_2.png'
+    },
+    {
+      title: 'Flexible schedule',
+      text: 'Plan your monthly workload and receive a steady stream of leads leads automatically',
+			desktopImage: 'benefit_3.png',
+			mobileImage: 'mobile_benefit_3.png'
+    },
+    {
+      title: 'A lot of work',
+      text: 'Browse leads to take on extra work as you have time',
+			desktopImage: 'benefit_1.png',
+			mobileImage: 'mobile_benefit_1.png'
+    },
+    {
+      title: 'Fast access',
+      text: 'Get first access to leads on subscription',
+			desktopImage: 'benefit_2.png',
+			mobileImage: 'mobile_benefit_2.png'
+    },
+    {
+      title: 'Ongoing support',
+      text: 'Home Improve offers you 24 hours phone and email support',
+			desktopImage: 'benefit_3.png',
+			mobileImage: 'mobile_benefit_3.png'
+    }
+  ];
+
 
   constructor(public securityService: SecurityService,
               public constants: Constants,
               public messages: Messages,
               private googleAnalyticsService: GoogleAnalyticsService,
               private hotJarService: HotJarService,
-              public scrollService: ScrollService) {
+              public scrollService: ScrollService,
+							private changeDetectorRef: ChangeDetectorRef,
+							private mediaQueryService: MediaQueryService) {
+
+		this.mediaQueryService.screen
+				.pipe(takeUntil(this.destroyed$))
+				.subscribe(mediaQuery => {
+					this.mediaQuery = mediaQuery;
+					if (mediaQuery.sm || mediaQuery.xs){
+						setTimeout(()=> {
+							this.swiper = new Swiper('.benefits-swiper', {
+								slidesPerView: 1.2,
+								spaceBetween: 30,
+								speed: 300,
+								loop: true,
+								breakpoints: {
+									450: {
+										slidesPerView: 1.2,
+										spaceBetween: 30
+									},
+									550: {
+										slidesPerView: 1.5,
+										spaceBetween: 30
+									},
+									650: {
+										slidesPerView: 1.8,
+										spaceBetween: 30
+									},
+									770: {
+										slidesPerView: 2,
+										spaceBetween: 30
+									}
+								}
+							});
+							this.changeDetectorRef.detectChanges();
+						},);
+					}
+				});
   }
 
   ngAfterViewInit(): void {
     this.hotJarService.tagRecording(['become a pro']);
+  }
+
+  selectBenefitImage(item) {
+    this.selectedBenefitImageUrl = item.desktopImage;
+    this.changeDetectorRef.detectChanges();
   }
 
   gTagTrackClick(): void {
--- browser/src/app/entity/home/pro-banner/pro-banner.component.ts
+++ browser/src/app/entity/home/pro-banner/pro-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SecurityService } from "../../../auth/security.service";
 import { Role } from '../../../model/security-model';
 
@@ -10,6 +10,14 @@ import { Role } from '../../../model/security-model';
 
 
 export class ProBannerComponent {
+
+  @Input() bannerConfig: any = {
+    image: 'pro-banner.png',
+    title: 'Find new customers and grow your business with Home Improve',
+    text: 'Whether you’re scaling your business, filling the gaps in your schedule, or just starting out, Home Improve will help you find the right customers.',
+    button: 'Become a PRO',
+    routerLink: '/become-pro'
+  };
 
   Role = Role;
   constructor(public securityService: SecurityService) {
