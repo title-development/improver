e7e4f57c94ede780f28ce9bff05ec884faa35930
Mykhailo Soltys <msoltys@codevision.com.ua>
2020-09-22 18:50:01 +0300

    IMP-1719: Fixed UI properties

--- browser/src/environments/environment.prod.ts
+++ browser/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   stripePublicKey: "pk_live_hWfv2z4uOMKLiDKikV2dAkZj00wioFdzo0",
+  googleApiKey: "AIzaSyDfNN72z0TVjEQqKftpCkQttwVIH_CboZY",
+  facebookClientId: '679075905790840',
+  googleClientId: '1078841939849-an19r4o58srgq6cu9d86flt70darg1f4.apps.googleusercontent.com',
   googleAnalyticsTrackingId: 'UA-148193868-3',
   hotJarTrackingId: 1490882,
+  googleReCaptchaSiteKey: '6Lc777YUAAAAAFh8rjprTYyJkduY6pcrK-UHB8uh',
+  captchaEnabled: true
 };
